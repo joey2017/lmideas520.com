f5c4210e24c6f411a919dc2711623a25da667ddc
joey <gxlclizongjun@163.com>
2019-03-19 19:45:32 +0800

    656

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 // 广告页面配置
 $adver = array('nkgg','qysq','tsyj','xwfkys','yysx','zrl');
 
-$token = isset($_REQUEST['cash']) ? $_REQUEST['cash'] : $adver[mt_rand(0,count($adver)-1)];
+//$token = isset($_REQUEST['cash']) ? $_REQUEST['cash'] : $adver[mt_rand(0,count($adver)-1)];
+//$token = $adver[mt_rand(0,count($adver)-1)];
+$token = isset($_REQUEST['cash']) ? $_REQUEST['cash'] : $adver[0];
 
 header('Location:/' . $token . '.html');
-
--- js/wx.js
+++ js/wx.js
@@ -1,4 +1,4 @@
-arr_wx = ['AXY6825','GFYS0328','GFYS1009','GFYS0818','GFYS5120'];
+arr_wx = ['tianjiale100','Ttxs_8030'];
 var wx_index = Math.floor((Math.random()*arr_wx.length));
 var stxlwx = arr_wx[wx_index];
 var wx_img_bottom ="<img src='" + "index_files/images/" + stxlwx +".jpg'" +" " + "/>";
