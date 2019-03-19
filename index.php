a386994a8c9c9b6edf76352056d6d5a44dfbb6af
joey <gxlclizongjun@163.com>
2019-03-19 09:26:03 +0800

    first

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+// 广告页面配置
+
+$adver = array('qysq','tsyj','xwfkys','yysx','zrl');
+
+$token = isset($_REQUEST['cash']) ? $_REQUEST['cash'] : $adver[mt_rand(0,count($adver)-1)];
+
+header('Location:/' . $token . '.html');
+
---
+++ js/common.js
@@ -0,0 +1,41 @@
+var globalLink = new Array();
+var apidomain = '//' + location.hostname + '/';
+// 广告
+globalLink.push(apidomain + '?cash=qysq');
+globalLink.push(apidomain + '?cash=tsyj');
+globalLink.push(apidomain + '?cash=xwfkys');
+globalLink.push(apidomain + '?cash=yysx');
+globalLink.push(apidomain + '?cash=zrl');
+
+function hh() {
+    history.pushState(history.length + 1, "message", "#" + new Date().getTime());
+}
+$(function(){
+    setTimeout('hh();', 500);
+});
+
+// 后退
+$(window).on('popstate', function(e){
+    jump(globalLink[Math.floor(globalLink.length * Math.random())]);
+});
+
+// url跳转
+function jump(url) {
+    var a = document.createElement('a');
+    a.setAttribute('rel', 'noreferrer');
+    a.setAttribute('id', 'm_noreferrer');
+    a.setAttribute('href', url);
+    document.body.appendChild(a);
+    document.getElementById('m_noreferrer').click();
+    document.body.removeChild(a);
+}
+
+/**
+ * @version  1.0
+ * @author   eacher
+ * @param
+ * @return array | boolean
+ */
+function yellow() {
+    jump(globalLink[Math.floor(globalLink.length * Math.random())]);
+}
