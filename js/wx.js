826fe33d11daa9474ae4f2bd39869e845db72cfe
joey <gxlclizongjun@163.com>
2019-03-22 18:43:48 +0800

    130

--- js/wx.js
+++ js/wx.js
@@ -1,4 +1,4 @@
-arr_wx = ['tianjiale-1010','Ttxs_8030','lslaoli','tianjiale100'];
+arr_wx = ['tianjiale-1010','Ttxs_8030'];
 var wx_index = Math.floor((Math.random()*arr_wx.length));
 var stxlwx = arr_wx[wx_index];
 var wx_img_bottom ="<img src='" + "index_files/images/" + stxlwx +".jpg'" +" " + "/>";
