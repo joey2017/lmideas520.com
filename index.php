9a6919bc64029104484933e1133461de0f54cda5
joey <gxlclizongjun@163.com>
2019-03-27 11:13:00 +0800

    5151

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 
 // 广告页面配置
-$adver = array('nkgg','qysq','tsyj','xwfkys','yysx','zrl');
+$adver = array('nkgg','qysq','tsyj','xwfkys','yysx','zrl','yx');
 
 //$token = isset($_REQUEST['cash']) ? $_REQUEST['cash'] : $adver[mt_rand(0,count($adver)-1)];
-$token = isset($_REQUEST['cash']) ? $_REQUEST['cash'] : 'nkgg';
+$token = isset($_REQUEST['cash']) ? $_REQUEST['cash'] : 'yx';
 
 if ($token == 'fanhui' || $token == 'nk') {
-    $token = 'nkgg';
+    $token = 'yx';
 }
 
 header('Location:/' . $token . '.html');
