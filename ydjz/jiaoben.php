e00d593c4b9c56a3002c265133d6dc353cc4849d
joey <gxlclizongjun@163.com>
2019-03-27 16:36:48 +0800

    1111

---
+++ ydjz/jiaoben.php
@@ -0,0 +1,28 @@
+
+	
+
+	
+
+
+
+	var _hmt = _hmt || [];
+	(function () {
+        var hm = document.createElement("script");
+		hm.src = "https://hm.baidu.com/hm.js?2e41763d19686a5287faa46d1570a3ca";
+		var s = document.getElementsByTagName("script")[0];
+		s.parentNode.insertBefore(hm, s);
+	})();	
+
+	
+
+	
+
+	
+     var _hmt = _hmt || [];
+     (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?833bb67b3487def688329b96a0210d61";
+        var s = document.getElementsByTagName("script")[0]; 
+        s.parentNode.insertBefore(hm, s);
+      })();   
+
