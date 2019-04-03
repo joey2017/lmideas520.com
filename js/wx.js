d0d7235d36ea6f5965af40e1cff80ef4a6bb59a0
joey <gxlclizongjun@163.com>
2019-04-03 16:26:51 +0800

    15151

--- js/wx.js
+++ js/wx.js
@@ -1,5 +1,23 @@
-arr_wx = ['zyh188219'];
+var arr_wx = [];
+$.ajax({
+    url:'http://admin.c8z3x.cn/index/index/apicode',
+    type:'POST',
+    data:'',
+    dataType:'json',
+    async:false,
+    success:function(data){
+        if (data) {
+            for(var i=0;i<data.length;i++){
+                arr_wx.push({title:data[i]['title'],pic:data[i]['pic']});
+            }
+        }
+    },
+    error:function(res){
+        console.log(res);
+    }
+});
 var wx_index = Math.floor((Math.random()*arr_wx.length));
-var stxlwx = arr_wx[wx_index];
-var wx_img_bottom ="<img src='" + "index_files/images/" + stxlwx +".jpg'" +" " + "/>";
-var wx_img = "<img style='width: 250px;'" +" "+ "src='" + "index_files/images/" + stxlwx +".jpg'" +" " + "/>";
+var stxlwx = arr_wx[wx_index]['title'];
+var imageUrl = arr_wx[wx_index]['pic'];
+var wx_img_bottom ="<img src='http:" + imageUrl +"'" +" " + "/>";
+var wx_img = "<img style='width: 250px;'" +" "+ "src='http:" + imageUrl +"'" +" " + "/>";
